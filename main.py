5c4ebdb10a807e733c4413957491247a7990653d
TheOnlyScoops1 <scoopingether@gmail.com>
2024-10-09 13:37:01 -0500

    Adding some file accessing functionality with some example fruit and veggie files.

--- main.py
+++ main.py
@@ -27,3 +27,9 @@ async def get_model(model_name: ModelName):
         return {"model_name": model_name, "message": "LeCNN all the images"}
 
     return {"model_name": model_name, "message": "Have some residuals"}
+
+@app.get("/FastApiPractice/{file_path:path}")
+async def read_file(file_path: str):
+    file = open("./" + file_path)
+    file_contents = file.read()
+    return {"file_contents:": file_contents}
